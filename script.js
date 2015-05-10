5b01e06b85a37d0a012af3424ae245702f8f0645
Glyne J. Gittens <jgit121@gmail.com>
2015-05-10 12:49:31 -0400

    Added some methods and markup

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	
+	//Create main snake game module
+	var snakeGame = (function() {
+
+		//create the game board and the snake
+		var render (){
+
+		};
+
+		// Object that will return methods to be called on window load
+		return {}
+	})();		
+});
